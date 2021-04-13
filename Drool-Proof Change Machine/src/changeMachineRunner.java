41c3becabd7c3f3072b0945ba782a160deebabd3
amandamuhr <amandamuhr@172-0-3-60.lightspeed.brhmal.sbcglobal.net>
2021-04-13 12:44:12 -0600

    Basic user questions set up

--- Drool-Proof Change Machine/src/changeMachineRunner.java
+++ Drool-Proof Change Machine/src/changeMachineRunner.java
@@ -13,9 +13,15 @@ public class changeMachineRunner
 		int payment = 0;
 		int change = 0;
 
-		//Getting info from user
+		//Choosing currency type
 		System.out.println("Welcome to the Drool-Proof Change Machine!");
+		System.out.println("Which currency type would you like to use?");
+		System.out.println("1. US");
+		System.out.println("2. Prime");
+		System.out.println("3. Binary");
+		System.out.println("4. Base-3");
 		System.out.println(" ");
+		//Getting info from user
 		System.out.println("How much money is your purchase worth? (Don't include decimals.)");
 		purchase = userInput.nextInt();
 		System.out.println("How much money did you give me?");
@@ -29,7 +35,7 @@ public class changeMachineRunner
 		System.out.println("Payment: " + payment);
 		System.out.println("Change Owed: " + change);
 		System.out.println("BREAKDOWN");
-		//Please work
+
 		
 	}
 
